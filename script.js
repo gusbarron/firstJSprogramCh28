1c5c283ff6f27f1f7e53620cde9ecde4ec465146
Gustavo Barron <gustavo.h.barron@gmail.com>
2022-04-02 11:56:12 -0700

    Class1

---
+++ script.js
@@ -0,0 +1,21 @@
+ //this is a comment in JS
+      //this is JS zone
+     // alert("Hello world");
+     // prompt("Enter your name");
+      console.log("Hello world");
+      document.write("<h6>Hello Gustavo</h6>");
+      console.warn("This is a warning");
+      console.error("Fatal warning ... but another computer");
+
+      var user; //initialization
+      user="Gustavo"; //assigning value
+      
+      var age=99;
+
+      let student="Astrid";
+      const taxes=0.11;
+      console.log(100*taxes);
+
+
+      document.write("<h6>Hello "+ student +"</h6>");
+      document.write(`<h6>Hello ${student}`)
